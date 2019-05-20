12e24c05cb19d36a7d4648ebca37c61608e91210
hockeygoalie78 <hockeygoalie7800email@gmail.com>
2019-05-20 14:50:33 -0400

    Rules update
    Added new rule to increase complexity

--- Assets/Scripts/CrypticPassword.cs
+++ Assets/Scripts/CrypticPassword.cs
@@ -19,11 +19,16 @@ public class CrypticPassword : MonoBehaviour {
     static MonoRandom RNG;
 
     private readonly string[] wordList = {
-        "PUZZLE", "ANSWER", "DEFUSE", "DANGER", "STRIKE", "EXPERT", "SELECT", "UNSURE", "BUTTON", "LIGHTS",
-        "THINGS", "MODULE", "DISARM", "MANUAL", "ALARMS", "NUMBER", "LETTER", "ASSETS", "CIPHER", "CAESAR",
-        "ATBASH", "DECODE", "FOLDER", "SHAPES", "EXPOSE", "PENCIL", "SUBMIT", "RANGES", "WINDOW", "VISUAL",
-        "ATTACH", "SAVING", "SEARCH", "COLORS", "ARROWS", "SCREEN", "STATIC", "STRING", "LISTEN", "SOLVES",
-        "EXCEPT", "ORDERS", "PUBLIC", "HOCKEY", "SLENDY", "SYSTEM", "MEMORY", "SKILLS", "SERIAL", "RANDOM"
+        "ANSWER", "EXPERT", "ALARMS", "ASSETS", "ATBASH", "ATTACH", "ARROWS", "EXCEPT", "ORDERS", "ERRORS",
+        "ASSIGN", "ALMOST", "ALWAYS", "ADJUST", "EITHER", "IMAGES", "ISSUES", "INPUTS", "INFORM", "UNLESS",
+        "ACCENT", "ACCEPT", "ACCESS", "IMAGES", "IMPACT", "IMPORT", "OPTION", "EXISTS", "EXPAND", "EVENTS",
+        "PUZZLE", "DEFUSE", "STRIKE", "MODULE", "DECODE", "SECURE", "SAMPLE", "BEWARE", "STROBE", "GAMBLE",
+        "JUMBLE", "MISUSE", "NOTICE", "PLEASE", "BEFORE", "VIRTUE", "VOLUME", "RESUME", "REDUCE", "MIDDLE",
+        "MENACE", "DOUBLE", "DIVIDE", "STUDIO", "DEVICE", "POTATO", "DEVOTE", "DERIVE", "STEREO", "PRINCE",
+        "UNSURE", "EXPOSE", "ESCAPE", "ENSURE", "UPDATE", "UNLIKE", "IGNORE", "ASSUME", "IMPOSE", "INCOME",
+        "DANGER", "SELECT", "BUTTON", "LIGHTS", "DISARM", "MANUAL", "NUMBER", "LETTER", "CIPHER", "CAESAR",
+        "FOLDER", "SHAPES", "SUBMIT", "RANGES", "VISUAL", "SEARCH", "COLORS", "SCREEN", "STATIC", "STRING",
+        "LISTEN", "SOLVES", "PUBLIC", "SYSTEM", "MEMORY", "SKILLS", "SERIAL", "RANDOM", "SWITCH", "SLICES"
     };
 
     private readonly char[][] charTable = new char[26][];
@@ -61,10 +66,31 @@ public class CrypticPassword : MonoBehaviour {
         var keyWord = KeyWordMesh.text = charList.Shuffle().Take(Random.Range(3, 7)).Join("");
         Debug.LogFormat(@"[Cryptic Password #{0}] Key word is: {1}", moduleId, keyWord);
 
+        //If the first letter of the start word is a vowel, reverse the key word
+        if("AEIOU".Contains(startWord[0]))
+        {
+            keyWord = keyWord.Reverse();
+            Debug.LogFormat(@"[Cryptic Password #{0}] Start word's first letter is a vowel. Key word is reversed.", moduleId);
+        }
+
+        //If the last letter of the start word is a vowel, swap the rows and columns of the cipher
+        bool lastLetterVowel = "AEIOU".Contains(startWord[5]);
+        if(lastLetterVowel)
+        {
+            Debug.LogFormat(@"[Cryptic Password #{0}] Start word's last letter is a vowel. Rows and columns of cipher are swapped.", moduleId);
+        }
+
         //Determine solution
         for (var c = 0; c < 6; c++) {
             //Calculates the solution based on the table cipher
-            solutionWord += charTable[keyWord[c % keyWord.Length] - 'A'][startWord[c] - 'A'];
+            if(lastLetterVowel)
+            {
+                solutionWord += charTable[startWord[c] - 'A'][keyWord[c % keyWord.Length] - 'A'];
+            }
+            else
+            {
+                solutionWord += charTable[keyWord[c % keyWord.Length] - 'A'][startWord[c] - 'A'];
+            }
 
             //Initialize letters
             displayLetters[c] = new List<char> {
@@ -142,7 +168,7 @@ public class CrypticPassword : MonoBehaviour {
         if (solutionWord.Equals(submissionWord)) {
             BombModule.HandlePass();
             moduleSolved = true;
-            Debug.LogFormat(@"[Cryptic Password #{0}] Module solved!");
+            Debug.LogFormat(@"[Cryptic Password #{0}] Module solved!", moduleId);
         } else {
             BombModule.HandleStrike();
             Debug.LogFormat(@"[Cryptic Password #{0}] That was incorrect. Submitted word was: {1}", moduleId, submissionWord);
